ac237a16dcb0e57dc59d260d444fb5046438a106
Beloded Darya <dariabeloded@gmail.com>
2019-01-07 11:03:11 +0300

    fix test

--- test.js
+++ test.js
@@ -5,7 +5,7 @@ const {getDate} = require('./server.js');
 
 it('правильно преобразует 2019-01-05T17:37:48.034Z', () => {
 	let date = new Date();
-	let expect = '2019-01-05'; // заменить на текущую дату
+	let expect = '2019-01-07'; // заменить на текущую дату
 	let curr = getDate(date);
   	assert.equal(curr, expect);
 });
